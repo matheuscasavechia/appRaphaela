ef05b5b320723b45d1acc0e49bdeb26f8f0254a0
Casavechia <matheus.casavechia@somei.com.vc>
2021-05-31 19:34:23 -0300

    instalando o componente de rotas e criando arquitetura para o projeto

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'native-base';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Esta funcionando essa bagaça!!!</Text>
+    </View>
+  );
+}
