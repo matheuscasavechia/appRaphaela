4296b869735cd63f49237c9de5022264cd25547f
Casavechia <matheus.casavechia@somei.com.vc>
2021-06-01 19:34:17 -0300

    colocando o degrade, formulario de login e botao para login

--- src/index.js
+++ src/index.js
@@ -1,18 +1,66 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'native-base';
+import {
+  View,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+} from 'react-native';
+import {Text, Input, Item, Label} from 'native-base';
 import LinearGradient from 'react-native-linear-gradient';
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{flex: 1}}>
       <LinearGradient
-        style={{ flex: 1 }}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 0, y: 1 }}
-        colors={['#FFCFF4', '#FB06C5']}>
-        <Text>Esta funcionando essa bagaça!!!</Text>
+        style={{flex: 1}}
+        start={{x: 0, y: 1}}
+        end={{x: 0, y: 0}}
+        colors={['#ffffff', '#FA69DA', '#FF41D5']}>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '50%',
+          }}>
+          <Text>LOGO DO APP AQUI</Text>
+        </View>
+
+        <View
+          style={{
+            height: 150,
+            justifyContent: 'center',
+            marginLeft: 16,
+            marginRight: 16,
+          }}>
+          <Item floatingLabel style={{borderColor: '#ffffff'}}>
+            <Label style={{color: '#ffffff'}}>Usuário</Label>
+            <Input style={{color: '#ffffff'}} />
+          </Item>
+          <Item floatingLabel style={{marginTop: 10, borderColor: '#ffffff'}}>
+            <Label style={{color: '#ffffff'}}>Senha</Label>
+            <Input style={{color: '#ffffff'}} />
+          </Item>
+        </View>
+
+        <TouchableOpacity
+          underlayColor="rgba(0, 0, 0, 0.2)"
+          useForeground
+          background={TouchableNativeFeedback.Ripple()}
+          onPress={() => {}}
+          style={{
+            height: 70,
+            width: '95%',
+            justifyContent: 'center',
+            alignSelf: 'center',
+            backgroundColor: '#ffffff',
+            borderRadius: 22,
+          }}>
+          <Text style={{textAlign: 'center'}}>Bora Entrar!</Text>
+        </TouchableOpacity>
       </LinearGradient>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
